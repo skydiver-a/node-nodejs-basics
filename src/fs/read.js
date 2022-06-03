1a4b19db76245c035193e4040fb3139b5a34145e
Anna Kravchenko <flamedance.ann@gmail.com>
2022-06-03 19:22:07 +0300

    feat: add 06-read.js decision

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,20 @@
+import { readdir, readFile } from 'node:fs';
+
 export const read = async () => {
-    // Write your code here 
+    // implement function that prints content of the fileToRead.txt
+    // into console (if there's no file fileToRead.txt
+    // Error with message FS operation failed must be thrown)
+
+    const targetFolder = 'files/';
+    const targetFile = 'fileToRead.txt';
+    readdir(targetFolder, {withFileTypes: true}, (err) => {
+        if (err) throw err;
+        readFile(targetFolder + targetFile, 'utf8', (err, data) => {
+            if (err) throw err;
+            console.log(data);
+        });
+    });
+
 };
+
+read();
