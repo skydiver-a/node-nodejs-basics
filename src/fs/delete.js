71aa3379045dd72b70daa3d766d9dc2b71245db3
Anna Kravchenko <flamedance.ann@gmail.com>
2022-06-03 14:31:44 +0300

    feat: add 04-delete.js decision

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,24 @@
+import { access, readdir, unlink } from 'node:fs';
+
 export const remove = async () => {
-    // Write your code here 
+    // implement function that deletes file fileToRemove.txt
+    // (if there's no file fileToRemove.txt
+    // Error with message FS operation failed must be thrown)
+    const targetFolder = 'files/';
+    const targetFile = 'fileToRemove.txt';
+
+    // if source folder doesn't exist
+    readdir(targetFolder, {withFileTypes: true}, (err) => {
+        if (err) throw err;
+    });
+    // if source file doesn't exist
+    access(targetFolder + targetFile, (err) => {
+        if (err) throw err;
+    });
+    // delete file
+    unlink(targetFolder + targetFile, (err) => {
+        if (err) throw err;
+    });
 };
+
+remove();
