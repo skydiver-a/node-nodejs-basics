c9734308738594dc705f8179a73f4eced0fb16c1
Anna Kravchenko <flamedance.ann@gmail.com>
2022-06-04 00:06:07 +0300

    feat: add 07-env.js decision

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,20 @@
+import { argv, stdout } from 'node:process';
+
 export const parseEnv = () => {
-    // Write your code here 
+    // implement function that parses environment variables
+    // with prefix RSS_ and prints them to the console in the format
+    // RSS_name1=value1; RSS_name2=value2
+    let str = '';
+    argv.slice(2)
+        .forEach((item, index, arr) => {
+            if (item.length >= 4 && item.toString().slice(0, 4) === 'RSS_') {
+                if (index+1 < arr.length) {
+                    str += `${item.toString()}=${arr[index+1]}; `;
+                }
+            }
+        })
+    stdout.write(str.slice(0, -2));
+    stdout.write('\n');
 };
+
+parseEnv();
