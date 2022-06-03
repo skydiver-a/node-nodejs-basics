197a163a1cfcfea174f0c8ee1a004d02f4fcadcd
Anna Kravchenko <flamedance.ann@gmail.com>
2022-06-03 17:50:39 +0300

    feat: add 05-list.js decision

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,20 @@
+import { readdir } from 'node:fs';
+
 export const list = async () => {
-    // Write your code here 
+    // implement function that prints all array of filenames
+    // from files folder into console (if files folder doesn't exists
+    // Error with message FS operation failed must be thrown)
+
+    const targetFolder = 'files/';
+
+    readdir(targetFolder, {withFileTypes: true}, (err, files) => {
+        if (err) throw err;
+        files.forEach(file => {
+            if (file.isFile()) {
+                console.log(`${file.name}`);
+            }
+        });
+    });
 };
+
+list();
