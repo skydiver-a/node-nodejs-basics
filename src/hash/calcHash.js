6ec79ca2ef89280b6b56c441168c30479306b78c
Anna Kravchenko <flamedance.ann@gmail.com>
2022-06-04 13:58:41 +0300

    feat: add 10-calcHash.js decision

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,23 @@
+import { createReadStream } from 'node:fs';
+import { createHash } from 'node:crypto';
+
 export const calculateHash = async () => {
-    // Write your code here 
+    // implement function that calculates SHA256 hash
+    // for file fileToCalculateHashFor.txt
+    // and return it as hex
+
+    // target file to stream
+    const targetStream = createReadStream('files/fileToCalculateHashFor.txt');
+    const hashSum = createHash('sha256').setEncoding('hex');
+
+    // hash sum calculates
+    targetStream.on('end', () => {
+        hashSum.end();
+        console.log(hashSum.read());
+    });
+
+    // file was read & was wrote to hash object
+    targetStream.pipe(hashSum);
 };
+
+calculateHash();
