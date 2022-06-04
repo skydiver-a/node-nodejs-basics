8d2ac89cdddcb24d8e374c646630ef718670c2f7
Anna Kravchenko <flamedance.ann@gmail.com>
2022-06-04 12:10:25 +0300

    feat: add 09-cjsToEsm.cjs decision

--- src/modules/cjsToEsm.cjs
+++ src/modules/cjsToEsm.cjs
@@ -1,3 +1,6 @@
+// rewrite it to it's equivalent in ECMAScript notation
+// (and switch extension to .mjs)
+
 const path = require('path');
 const { release, version } = require('os');
 const { createServer: createServerHttp } = require('http');
