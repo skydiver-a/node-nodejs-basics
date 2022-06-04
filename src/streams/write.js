2029ac9772a633c82e853c2710792d0161c0d3fc
Anna Kravchenko <flamedance.ann@gmail.com>
2022-06-04 21:03:46 +0300

    feat: add 12-write.js decision

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,33 @@
+import { createWriteStream } from 'node:fs';
+import { createInterface } from 'readline';
+
 export const write = async () => {
-    // Write your code here 
+    // implement function that writes process.stdin data
+    // into file fileToWrite.txt content using Writable Stream
+
+    // target file to write data
+    const writableStream = createWriteStream('files/fileToWrite.txt', {encoding: 'utf8'});
+    // interface to read data from console
+    const readLines = createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    readLines.on('line', (line) => {
+        switch (line.trim()) {
+            case 'exit':
+                readLines.close();
+                break;
+            default:
+                writableStream.write(line + '\n');
+                break;
+        }
+    }).on('close', () => {
+        writableStream.end();
+        setTimeout(() => {
+            process.exit(0);
+        }, 100);
+    });
 };
+
+write();
