4ebb7b2533239e0870f4ef50146292e71fe5702d
Anna Kravchenko <flamedance.ann@gmail.com>
2022-06-04 09:52:54 +0300

    fix: fix 07-env.js decision for object .env

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,9 @@
 export const parseArgs = () => {
-    // Write your code here 
+    // implement function that parses command line arguments
+    // (given in format --propName value --prop2Name value2,
+    // you don't need to validate it) and prints them
+    // to the console in the format
+    // propName is value, prop2Name is value2
 };
+
+parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,18 +1,16 @@
-import { argv, stdout } from 'node:process';
+import { env, stdout } from 'node:process';
 
 export const parseEnv = () => {
     // implement function that parses environment variables
     // with prefix RSS_ and prints them to the console in the format
     // RSS_name1=value1; RSS_name2=value2
+
     let str = '';
-    argv.slice(2)
-        .forEach((item, index, arr) => {
-            if (item.length >= 4 && item.toString().slice(0, 4) === 'RSS_') {
-                if (index+1 < arr.length) {
-                    str += `${item.toString()}=${arr[index+1]}; `;
-                }
-            }
-        })
+    Object.entries(env).forEach(([key, value] = entry) => {
+        if (key.toString().slice(0, 4) === 'RSS_') {
+            str += `${key}=${value}; `;
+        }
+    });
     stdout.write(str.slice(0, -2));
     stdout.write('\n');
 };
