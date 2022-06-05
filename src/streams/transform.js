425f615b6df6f58b08bc5dc3a2ed3a1eccea8f4c
Anna Kravchenko <flamedance.ann@gmail.com>
2022-06-05 22:09:35 +0300

    feat: add 13-transform.js decision

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,25 @@
+import { Transform } from 'node:stream';
+
 export const transform = async () => {
-    // Write your code here 
+    // implement function that reads data from process.stdin,
+    // reverses text using Transform Stream
+    // and then writes it into process.stdout
+
+    const reverseTextStream = new Transform({
+        transform: (chunk, encoding, callback) => {
+            try {
+                const reverseText = `${chunk.toString('utf8')
+                                        .split('')
+                                        .reverse()
+                                        .join('')}`;
+                callback(null, reverseText);
+            } catch (err) {
+                callback(err);
+            }
+        }
+    });
+
+    process.stdin.pipe(reverseTextStream).pipe(process.stdout);
 };
+
+transform();
